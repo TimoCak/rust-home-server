de955203b2604b21038f3287a961b1099ad68a74
Timo Caktu <timocaktu@gmail.com>
2024-03-08 00:43:49 +0100

    added logger to project

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use actix_files::Files;
-use actix_web::{get, http::header::ContentType, App, HttpResponse, HttpServer};
+use actix_web::{
+    get,
+    http::header::ContentType,
+    middleware::Logger,
+    web::{self, get, resource},
+    App, HttpRequest, HttpResponse, HttpServer,
+};
+use env_logger::Env;
 
-#[get("/hello")]
 async fn hello() -> HttpResponse {
     HttpResponse::Ok()
         .content_type(ContentType::html())
@@ -22,16 +28,35 @@ async fn weather_page() -> HttpResponse {
         .body(include_str!("../assets/weather.html"))
 }
 
+async fn get_path(req: HttpRequest) -> HttpResponse {
+    let url = req.resource_map();
+    println!("{:?}", url);
+    HttpResponse::Ok().finish()
+}
+
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        resource("/users/{id}")
+            .name("users_user_by_id")
+            .route(web::get().to(get_path)),
+    );
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     HttpServer::new(|| {
         App::new()
-            .service(hello)
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .configure(config)
+            .service(resource("/hello").name("hello").route(get().to(hello)))
             .service(index_page)
             .service(weather_page)
             .service(Files::new("/assets", "./assets").prefer_utf8(true))
     })
-    .bind(("localhost", 8080))?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
