1643b15228747ae1b90ab1652570cf3f4cc8bd81
Timo Caktu <timocaktu@gmail.com>
2024-05-19 02:02:55 +0200

    added a music playlist page to the prject

--- src/api_calls/finance.rs
+++ src/api_calls/finance.rs
@@ -1,3 +1,8 @@
+use actix_web::HttpResponse;
+
 /*
 finance handlers
 */
+pub(crate) async fn get_Balance() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,13 @@ async fn fincance_page() -> impl Responder {
         .body(include_str!("../assets/finance.html"))
 }
 
+#[get("/playlist")]
+async fn playlist_page() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(include_str!("../assets/playlist.html"))
+}
+
 async fn get_path(req: HttpRequest) -> HttpResponse {
     let url = req.resource_map();
     println!("{:?}", url);
@@ -55,6 +62,7 @@ async fn main() -> std::io::Result<()> {
             .service(index_page)
             .service(weather_page)
             .service(fincance_page)
+            .service(playlist_page)
             .service(Files::new("/assets", "./assets").prefer_utf8(true))
     })
     .bind(("127.0.0.1", 8080))?
